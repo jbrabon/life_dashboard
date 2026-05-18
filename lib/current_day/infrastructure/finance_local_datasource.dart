b884c1e892cbf0c0dede44b39834fe4e0f13f1cc
jbrabon <jbrabon13@gmail.com>
2026-05-17 23:23:25 -0500

    Add finance expense datasource methods

--- lib/current_day/infrastructure/finance_local_datasource.dart
+++ lib/current_day/infrastructure/finance_local_datasource.dart
@@ -27,6 +27,31 @@ class FinanceLocalDatasource {
     );
   }
 
+  Future<void> insertExpenseEntry({
+    required String id,
+    required String daySessionId,
+    required int amountCents,
+    String? category,
+    String? note,
+    required String occurredAtUtc,
+    required String createdAtUtc,
+  }) async {
+    final db = await _db;
+
+    await db.insert(
+      'finance_expense_entries',
+      {
+        'id': id,
+        'day_session_id': daySessionId,
+        'amount_cents': amountCents,
+        'category': category,
+        'note': note,
+        'occurred_at_utc': occurredAtUtc,
+        'created_at_utc': createdAtUtc,
+      },
+    );
+  }
+
   Future<int> getIncomeTotalCentsForDaySession(String daySessionId) async {
     final db = await _db;
 
@@ -34,6 +59,21 @@ class FinanceLocalDatasource {
       '''
 SELECT COALESCE(SUM(amount_cents), 0) AS total_cents
 FROM finance_income_entries
+WHERE day_session_id = ?
+''',
+      [daySessionId],
+    );
+
+    return result.first['total_cents'] as int;
+  }
+
+  Future<int> getExpenseTotalCentsForDaySession(String daySessionId) async {
+    final db = await _db;
+
+    final result = await db.rawQuery(
+      '''
+SELECT COALESCE(SUM(amount_cents), 0) AS total_cents
+FROM finance_expense_entries
 WHERE day_session_id = ?
 ''',
       [daySessionId],
